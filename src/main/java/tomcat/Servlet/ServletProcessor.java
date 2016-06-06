28ea1a4f46bb97d189000f395d075da80f817f98
harlenzhang <hanwen.zhang@tqmall.com>
2016-06-06 16:11:36 +0800

    update

---
+++ src/main/java/tomcat/Servlet/ServletProcessor.java
@@ -0,0 +1,22 @@
+package tomcat.Servlet;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandler;
+
+/**
+ * Created by harlenzhang on 16/6/6.
+ */
+public class ServletProcessor {
+
+    public void process(Request request, Response response){
+        String uri = request.getUri();
+        String servletName = uri.substring(uri.lastIndexOf("/") + 1);
+        URLClassLoader loader;
+
+        URL[] urls = new URL[];
+        URLStreamHandler streamHandler;
+
+    }
+}
